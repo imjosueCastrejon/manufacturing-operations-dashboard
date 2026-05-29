41fa53e644305bb7ad108c73e2efe68ebf3b44f7
josueC <j.armandocastrejon@gmail.com>
2026-05-28 22:36:01 -0600

    feat: configure postgres

--- manufacturing-operations-dashboard-backend/src/app.module.ts
+++ manufacturing-operations-dashboard-backend/src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { WorkOrdersModule } from './work-orders/work-orders.module';
 import { QualityIssuesModule } from './quality-issues/quality-issues.module';
 import { ProductionMetricsModule } from './production-metrics/production-metrics.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [WorkOrdersModule, QualityIssuesModule, ProductionMetricsModule],
+  imports: [
+    WorkOrdersModule,
+    QualityIssuesModule,
+    ProductionMetricsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5435,
+      username: 'postgres',
+      password: '',
+      database: 'manufacturing_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
