9e0596897e241ebf4d438516dd573c605cc1dbd8
josueC <j.armandocastrejon@gmail.com>
2026-05-31 14:29:26 -0600

    feat: add quality issue entity and DTOs

--- manufacturing-operations-dashboard-backend/src/quality-issues/dto/create-quality-issue.dto.ts
+++ manufacturing-operations-dashboard-backend/src/quality-issues/dto/create-quality-issue.dto.ts
@@ -1,1 +1,18 @@
-export class CreateQualityIssueDto {}
+import { IsString, IsNotEmpty, IsEnum } from "class-validator";
+import { QualityIssueSeverity, QualityIssueStatus } from "src/common/enums/status.enum";
+
+export class CreateQualityIssueDto {
+  @IsString()
+  @IsNotEmpty()
+  title!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  area!: string;
+
+  @IsEnum(QualityIssueSeverity)
+  severity!: QualityIssueSeverity;
+
+  @IsEnum(QualityIssueStatus)
+  status!: QualityIssueStatus;
+}
--- manufacturing-operations-dashboard-backend/src/quality-issues/dto/update-quality-issue.dto.ts
+++ manufacturing-operations-dashboard-backend/src/quality-issues/dto/update-quality-issue.dto.ts
@@ -1,4 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateQualityIssueDto } from './create-quality-issue.dto';
+import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
+import { QualityIssueSeverity, QualityIssueStatus } from 'src/common/enums/status.enum';
 
-export class UpdateQualityIssueDto extends PartialType(CreateQualityIssueDto) {}
+export class UpdateQualityIssueDto extends PartialType(CreateQualityIssueDto) {
+  @IsString()
+  @IsNotEmpty()
+  title!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  area!: string;
+
+  @IsEnum(QualityIssueSeverity)
+  severity!: QualityIssueSeverity;
+
+  @IsEnum(QualityIssueStatus)
+  status!: QualityIssueStatus;
+}
