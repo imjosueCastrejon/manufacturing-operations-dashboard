d852696dc1d1e7977ef636bf81bb63ae9a9508eb
josueC <j.armandocastrejon@gmail.com>
2026-05-31 14:30:02 -0600

    feat: add work-order entity and DTOs

--- manufacturing-operations-dashboard-backend/src/work-orders/entities/work-order.entity.ts
+++ manufacturing-operations-dashboard-backend/src/work-orders/entities/work-order.entity.ts
@@ -1,1 +1,36 @@
-export class WorkOrder {}
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { WorkOrderStatus } from '../../common/enums/status.enum';
+import { ProductionMetric } from '../../production-metrics/entities/production-metric.entity';
+
+@Entity('work_orders')
+export class WorkOrder {
+  
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id!: string; // BIGINT se maneja como string en TS para evitar pérdida de precisión
+
+  
+  @Column({ name: 'order_number', type: 'text', unique: true })
+  orderNumber!: string;
+
+  @Column({ name: 'product_name', type: 'text' })
+  productName!: string;
+
+  @Column({ name: 'quantity_target', type: 'integer' })
+  quantityTarget!: number;
+
+  @Column({
+    type: 'text',
+    enum: WorkOrderStatus,
+  })
+  status!: WorkOrderStatus;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
+  updatedAt!: Date;
+
+  
+  @OneToMany(() => ProductionMetric, (metric) => metric.workOrder)
+  metrics!: ProductionMetric[];
+}
