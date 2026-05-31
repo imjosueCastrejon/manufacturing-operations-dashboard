4426988391df1490459893b6c21191832bb9b629
josueC <j.armandocastrejon@gmail.com>
2026-05-31 14:28:30 -0600

    feat: add production entity and DTOs

--- manufacturing-operations-dashboard-backend/src/production-metrics/dto/create-production-metric.dto.ts
+++ manufacturing-operations-dashboard-backend/src/production-metrics/dto/create-production-metric.dto.ts
@@ -1,1 +1,11 @@
-export class CreateProductionMetricDto {}
+import { IsNumberString, IsNotEmpty, IsInt, Min } from "class-validator";
+
+export class CreateProductionMetricDto {
+  @IsNumberString() // Validamos como string numérico ya que el ID de origen es BIGINT
+  @IsNotEmpty()
+  workOrderId!: string;
+
+  @IsInt()
+  @Min(1)
+  unitsProduced!: number;
+}
--- manufacturing-operations-dashboard-backend/src/production-metrics/entities/production-metric.entity.ts
+++ manufacturing-operations-dashboard-backend/src/production-metrics/entities/production-metric.entity.ts
@@ -1,1 +1,22 @@
-export class ProductionMetric {}
+import { WorkOrder } from "src/work-orders/entities/work-order.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('production_metrics')
+export class ProductionMetric {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id!: string;
+
+  @Column({ name: 'work_order_id', type: 'bigint' })
+  workOrderId!: string;
+
+  @Column({ name: 'units_produced', type: 'integer' })
+  unitsProduced!: number;
+
+  @CreateDateColumn({ name: 'recorded_at', type: 'timestamptz' })
+  recordedAt!: Date;
+
+  // Relación Muchos a Uno con WorkOrders (ON DELETE CASCADE)
+  @ManyToOne(() => WorkOrder, (workOrder) => workOrder.metrics, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'work_order_id' })
+  workOrder!: WorkOrder;
+}
